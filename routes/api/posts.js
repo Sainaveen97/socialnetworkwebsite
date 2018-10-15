eb7bbcfa7251ff2d3cdd5745e04d175c2b6de493
Naveen Vemuri <naveen.vemuri@ggktech.local>
2018-10-15 11:14:51 +0530

    Posts routes completed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -139,15 +139,50 @@ router.post(
   "/comment/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
     Post.findById(req.params.id)
       .then(post => {
         const newComment = {
-          text: req.body.tex,
+          text: req.body.text,
           name: req.body.name,
           avatar: req.body.avatar,
           user: req.user.id
         };
         post.comments.unshift(newComment);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No Post found" }));
+  }
+);
+
+//@route POST api/posts/comment/:id/:comment_id
+//@desc Delete comment from post
+//@access Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        //Check if comment exist
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return releaseEvents
+            .status(404)
+            .json({ commentsnotexists: "Comment does not exist" });
+        }
+        //Get remove Index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+        //Splice comment out of array
+        post.comments.splice(removeIndex, 1);
         post.save().then(post => res.json(post));
       })
       .catch(err => res.status(404).json({ postnotfound: "No Post found" }));
